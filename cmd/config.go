4828a4712bf07e97ac7d2fb47b8e008f3255fca6
Addanus <83732318+addanus@users.noreply.github.com>
2021-05-23 15:50:51 -0400

    invalid parameters fixes

--- cmd/config.go
+++ cmd/config.go
@@ -14,9 +14,9 @@ type Config struct {
 func LoadConfig() *Config {
 	config := Config{}
 
-	config.MongoURI = viper.GetString("monog-uri")
-	config.MongoDatabase = viper.GetString("monog-database")
-	config.MongoCollection = viper.GetString("monog-collection")
+	config.MongoURI = viper.GetString("mongo-uri")
+	config.MongoDatabase = viper.GetString("mongo-database")
+	config.MongoCollection = viper.GetString("mongo-collection")
 
 	return &config
 }
